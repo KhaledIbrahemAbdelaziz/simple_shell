4e9e0c4fa8c033d9457472ae5ba20396dd96aa1f
mikechege01 <mike@mail.com>
2023-04-26 12:12:02 -0700

    New Task 9 senv unset

--- shell.h
+++ shell.h
@@ -14,13 +14,24 @@
 #define MAX_COMMAND_LENGTH 100
 #define MAX_LINE 1024
 #define BUFFER_SIZE 1024
-
+/*environ*/
 extern char **environ;
+/*logic functions*/
+int run(char **args, int flag);
+int run_cmd(char **args, int flag);
+/*seperator function*/
 int new_proc(char **args);
 int command_sep(char **args);
 char **parse_command(char *line);
+/*built in functions*/
+int builtin(char *cmd);
+int run_builtin(int index);
+int run_normal(char **args, int flag);
+int run(char **args, int flag);
 int execute_builtin_command(char **args, char *buf);
+/*change directory funtion*/
 void my_cd(char **args, char **env __attribute__((unused)));
+
 void prompt(char *argv[], char *env[]);
 void exits(char *argv[], char *buf);
 void Env(char *env[]);
