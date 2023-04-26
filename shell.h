31d38ad480ce100efbf39fe60caca1ac058efeee
mikechege01 <mike@mail.com>
2023-04-26 12:16:00 -0700

    modified header

--- shell.h
+++ shell.h
@@ -24,10 +24,6 @@ int new_proc(char **args);
 int command_sep(char **args);
 char **parse_command(char *line);
 /*built in functions*/
-int builtin(char *cmd);
-int run_builtin(int index);
-int run_normal(char **args, int flag);
-int run(char **args, int flag);
 int execute_builtin_command(char **args, char *buf);
 /*change directory funtion*/
 void my_cd(char **args, char **env __attribute__((unused)));
