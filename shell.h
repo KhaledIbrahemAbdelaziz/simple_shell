a282aefc15b58f20844bb14a44114a94a2152494
mikechege01 <mike@mail.com>
2023-04-27 08:50:58 -0700

    restored features

--- shell.h
+++ shell.h
@@ -20,24 +20,9 @@
 #define BUFFER_SIZE 1024
 /*environ*/
 extern char **environ;
-/**
- * struct passdata - carries arguments to pass into a function
- * @arg: arguments
- * @argv: path to the current cmd
- * @runfd: fd to read line input
- */
-typedef struct passdata
-{
-	int runfd;
-} data_r;
-
-int character(int i);
-int checker(char i, char *delimiter);
-int command_mode(data_r *data);
-void prompt(data_r *data);
-int non_interactive(data_r *data);
 void exits(char *argv[], char *buf);
 void Env(char *env[]);
+void prompt(char *argv[], char *env[]);
 int strcompare(char *str1, char *str2);
 int strlength(char *str);
 
