7a42beeee34467d19c640e48e050791314bf3f18
mikechege01 <mike@mail.com>
2023-04-27 04:31:35 -0700

    Task 1,2,4,5

--- prompt.c
+++ prompt.c
@@ -19,8 +19,8 @@ int check_mode(void)
 
 void prompt(char *argv[], char *env[])
 {
-	char *buffer, *token[] = {NULL, NULL};
-	size_t size;
+	char *buffer = NULL, *token[1024] = {NULL};
+	size_t size = 0;
 	ssize_t command;
 	int stat, y, x;
 	pid_t proc_child;
@@ -29,7 +29,6 @@ void prompt(char *argv[], char *env[])
 	{
 		if (check_mode())
 			printf("#cisfun$ ");
-		buffer = malloc(sizeof(char *) * size);
 		command = getline(&buffer, &size, stdin);
 		if (command == -1)
 		{
@@ -45,7 +44,7 @@ void prompt(char *argv[], char *env[])
 		}
 		x = 0;
 		token[x] = strtok(buffer, " ");
-		while (token[x])
+		while (token[x] && x < 1023)
 			token[++x] = strtok(NULL, " ");
 		proc_child = fork();
 		if (proc_child == -1)
@@ -57,18 +56,20 @@ void prompt(char *argv[], char *env[])
 		{
 			if (x > 1)
 			{
-				if (execve(token[0], token, env) == -1)
-					printf("%s: No such file or directory\n", argv[0]);
+				if (execvp(token[0], token) == -1)
+					printf("%s: Command not found", token[0]);
 			}
-			else if (strcompare(token[0], "exit"))
+			else if (strcompare(token[0], "exit") == 0)
 				exits(argv, buffer);
-			else if (strcompare(token[0], "env"))
+			else if (strcompare(token[0], "env") == 0)
 				Env(env);
 			else
 			{
-				if (execve(token[0], token, env) == -1)
-					printf("%s: No such file or directory\n", argv[0]);
+				if (execvp(token[0], token) == -1)
+					printf("%s: Command not found\n", token[0]);
 			}
+			free(buffer);
+			exit(0);
 		}
 		else
 			wait(&stat);
@@ -84,12 +85,23 @@ void prompt(char *argv[], char *env[])
 
 int strcompare(char *str1, char *str2)
 {
-	int i;
-
-	for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++)
+	if (str1 == NULL || str2 == NULL)
 	{
-		if (str1[i] != str2[i])
-			return ((int)str1[i] - str2[i]);
+		return (1);
+	}
+	while (*str1 && *str2)
+	{
+		if (*str1 != *str2)
+		{
+			return (*str1 - *str2);
+		}
+		str1++;
+		str2++;
+	}
+	if (*str1 != *str2)
+	{
+		return (*str1 - *str2);
 	}
 	return (0);
 }
+
