100515c923b443763369926d3baf4c2f03be6f96
mikechege01 <mike@mail.com>
2023-04-27 01:14:07 -0700

    modified shell.h

--- shell.h
+++ shell.h
@@ -9,10 +9,13 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <limits.h>
+#include <ctype.h>
 #include <errno.h>
 
 #define MAX_COMMAND_LENGTH 100
 #define MAX_LINE 1024
+#define HISTORY_MAXITEMS 100
+#define ARG_MAX_COUNT 1024
 #define BUFFER_SIZE 1024
 /*environ*/
 extern char **environ;
