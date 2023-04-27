5d23455bd01b43b99fff0f993dbe7fd66d9136d2
mikechege01 <mike@mail.com>
2023-04-27 13:53:20 -0700

    betty fixed file

---
+++ Home.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * cd - Changes the current working directory
+ * @args: The dir to change to.
+ * Return: 1 one success, 0 otherwise.
+ */
+int cd(char **args)
+{
+	if (args[1] == NULL)
+	{
+		fprintf(stderr, "Expected argument to \"cd\"\n");
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror("Error in cd.c: Changing dir\n");
+		}
+	}
+	return (-1);
+}
