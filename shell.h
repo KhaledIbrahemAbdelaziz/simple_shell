9dc9eb86d5f5d1df7d23269d08b2134149cb1834
mikechege01 <mike@mail.com>
2023-04-27 13:48:37 -0700

    Modified shell - header

--- shell.h
+++ shell.h
@@ -1,28 +1,34 @@
 #ifndef SHELL_H
 #define SHELL_H
-
+/*All the required libraries*/
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <limits.h>
 #include <ctype.h>
 #include <errno.h>
-
-#define MAX_COMMAND_LENGTH 100
-#define MAX_LINE 1024
-#define ARG_MAX_COUNT 1024
+#include <sys/wait.h>
+/*delimiter def*/
+#define TOK_DELIM " \t\r\n\a\""
+/*BUFFER_SIZE def*/
 #define BUFFER_SIZE 1024
-
-/*environ*/
+/*environ definition*/
 extern char **environ;
-void exits(char *argv[], char *buf);
-void Env(char *env[]);
-void prompt(char *argv[], char *env[]);
-int strcompare(char *str1, char *str2);
-int strlength(char *str);
-
-#endif /* SHELL_H */
+/*Task: interactive*/
+void user_interactive(void);
+/* Input and strings*/
+char *read_input(void);
+char **lines_sp(char *line);
+int run_coms(char **args);
+/* Forking and processes  */
+int forks(char **args);
+/* non - interactive mode fun*/
+char *stream(void);
+void tok_en(void);
+/*The builtin fuctions*/
+int cd(char **args);
+int my_exit(char **args);
+int env(char **args);
+int help(char **args);
+#endif/*_SHELL_H_*/
